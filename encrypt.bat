431372e3fa2e4fcd87dddf9275a55ef1a6cffa52
Pablo Cumpián Díaz <pablo@MacBook-Pro-de-Pablo.local>
2017-08-03 00:38:03 +0200

    now the scripts goes back to the root dir

--- encrypt.bat
+++ encrypt.bat
@@ -4,6 +4,8 @@ set root=%1
 set direc=%cd%
 set jar=%cd%\decrypt-translation-util-3.0.0.jar
 
+set number=0
+
 call :treeProcess
 goto :eof
 
@@ -11,6 +13,8 @@ goto :eof
 
 for %%f in (*.xml) do (
     java -cp %jar% com.claytablet.cq5.ctctranslation.Utils.Crypt.Encrypt -s %direc%\ctt-keystore.jceks -w aJ3i49sjwF -i %%f -a clay-tablet-keys -p aJ3i49sjwF
+
+    set /a "number=%number%+1"
 )
 
 for /D %%d in (*) do (
@@ -24,3 +28,5 @@ for /D %%d in (*) do (
 exit /b
 
 cd %direc%
+
+echo encriptados %number% archivo(s)
