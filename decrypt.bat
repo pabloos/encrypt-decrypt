8af32e4debfa74e7fcda4dee7c607ab4792fcede
Pablo Cumpián Díaz <pablo@MacBook-Pro-de-Pablo.local>
2017-08-02 14:34:37 +0200

    now the scripts goes back to the root dir

--- decrypt.bat
+++ decrypt.bat
@@ -1,17 +1,23 @@
 @echo off
 
+set root=%1
+
 set direc=%cd%
 set jar=%cd%\decrypt-translation-util-3.0.0.jar
 
-echo %jar%
+cd %root%
 
 call :treeProcess
+
+cd %root%
+
 goto :eof
 
 :treeProcess
 
 for %%f in (*.xml) do (
-    java -cp %jar% com.claytablet.cq5.ctctranslation.Utils.Crypt.Encrypt -s %direc%\ctt-keystore.jceks -w aJ3i49sjwF -i %%f -a clay-tablet-keys -p aJ3i49sjwF
+    echo entrando en %cd%
+    java -cp %jar% com.claytablet.cq5.ctctranslation.Utils.Crypt.Decrypt -s %direc%\ctt-keystore.jceks -w aJ3i49sjwF -i %%f -a clay-tablet-keys -p aJ3i49sjwF
 )
 
 for /D %%d in (*) do (
--- encrypt.bat
+++ encrypt.bat
@@ -5,16 +5,18 @@ set root=%1
 set direc=%cd%
 set jar=%cd%\decrypt-translation-util-3.0.0.jar
 
-echo %jar%
-
 cd %root%
 
 call :treeProcess
+
+cd %root%
+
 goto :eof
 
 :treeProcess
 
 for %%f in (*.xml) do (
+    echo entrando en %cd%
     java -cp %jar% com.claytablet.cq5.ctctranslation.Utils.Crypt.Encrypt -s %direc%\ctt-keystore.jceks -w aJ3i49sjwF -i %%f -a clay-tablet-keys -p aJ3i49sjwF
 )
 
