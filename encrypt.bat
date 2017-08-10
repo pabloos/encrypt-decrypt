c045c1a5b31111f86d1a4efcfb813ab1c5c69b68
Pablo <pcumpian1@gmail.com>
2017-08-10 23:21:17 +0200

    counting and listing files processed

--- encrypt.bat
+++ encrypt.bat
@@ -4,6 +4,8 @@ set root=%1
 set direc=%cd%
 set jar=%cd%\decrypt-translation-util-3.0.0.jar
 
+set /a fileCount=0
+
 cd %root%
 
 call :treeProcess
@@ -13,6 +15,10 @@ goto :eof
 
 for %%f in (*.xml) do (
     java -cp %jar% com.claytablet.cq5.ctctranslation.Utils.Crypt.Encrypt -s %direc%\ctt-keystore.jceks -w aJ3i49sjwF -i %%f -a clay-tablet-keys -p aJ3i49sjwF
+
+    echo %%i
+
+    set /a fileCount=%fileCount% + 1
 )
 
 for /D %%d in (*) do (
@@ -27,4 +33,6 @@ exit /b
 
 cd %direc%
 
-echo "fin"
+echo encrypted %fileCount% file(s)
+
+echo 
